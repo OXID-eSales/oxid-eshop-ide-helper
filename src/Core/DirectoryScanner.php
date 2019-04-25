ea690d76f35fdf2a97f91f99cf460443b97e8447
hkreuter <heike@reuter-consultig.org>
2019-04-25 15:20:58 +0200

    OXDEV-1561 Add DirectoryScanner and test

---
+++ src/Core/DirectoryScanner.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+namespace OxidEsales\EshopIdeHelper\Core;
+
+use \Webmozart\PathUtil\Path;
+
+/**
+ * Class DirectoryScanner: Recursively scan given path for matching files (case insensitive).
+ *
+ * @package OxidEsales\EshopIdeHelper\Core
+ */
+class DirectoryScanner
+{
+    /**
+     * @var array
+     */
+    private $filePaths = [];
+
+    /**
+     * @var string
+     */
+    private $searchForFileName = '';
+
+    /**
+     * @var string
+     */
+    private $startPath = '';
+
+    /**
+     * DirectoryScanner constructor.
+     *
+     * @param string $searchForFileName
+     * @param string $startPath
+     */
+    public function __construct($searchForFileName, $startPath)
+    {
+        $this->searchForFileName = strtolower($searchForFileName);
+        $this->startPath = $startPath;
+    }
+
+    /**
+     * Scan shop modules directory.
+     *
+     * @return array
+     */
+    public function getFilePaths()
+    {
+        $this->scanDirectory($this->startPath);
+
+        return $this->filePaths;
+    }
+
+    /**
+     * Recursive search for matching files.
+     *
+     * @param string $clearFolderPath Sub-folder path to check for search file name.
+     */
+    private function scanDirectory($directoryPath)
+    {
+        if (is_dir($directoryPath)) {
+            $files = scandir($directoryPath);
+            foreach ($files as $fileName) {
+                $filePath = Path::join($directoryPath, $fileName);
+                if (is_dir($filePath) && !in_array($fileName, ['.', '..'])) {
+                    $this->scanDirectory($filePath);
+                } elseif ($this->searchForFileName == strtolower($fileName)) {
+                    $this->filePaths[] = $filePath;
+                }
+            }
+        }
+    }
+}
---
+++ tests/Unit/DirectoryScannerTest.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+namespace OxidEsales\EshopIdeHelper\tests\Unit;
+
+use OxidEsales\EshopIdeHelper\Core\DirectoryScanner;
+
+/**
+ * Class DirectoryScannerTest
+ *
+ * @package OxidEsales\EshopIdeHelper\tests\Unit
+ */
+class DirectoryScannerTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * Test case that provided directory does not exist.
+     */
+    public function testScanNotExistingDirectory()
+    {
+        $scanner = new DirectoryScanner('DirectoryScannerTest.php', 'not_existing_path');
+        $this->assertEmpty($scanner->getFilePaths());
+    }
+
+    /**
+     * Test case that provided file does not exist.
+     */
+    public function testScanFileNameNotSet()
+    {
+        $scanner = new DirectoryScanner('not_existing_file', dirname(__DIR__));
+        $this->assertEmpty($scanner->getFilePaths());
+    }
+
+    /**
+     * Test success case.
+     */
+    public function testScanForFilesSuccess()
+    {
+        $scanner = new DirectoryScanner('DirectoryScannerTest.php', dirname(__DIR__));
+        $this->assertEquals(1, count($scanner->getFilePaths()));
+    }
+}
