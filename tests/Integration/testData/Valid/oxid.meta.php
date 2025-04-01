a4fa8a6a63b15550b7fa13b25abf49b0a90fe430
Maximilian Bösing <2189546+boesing@users.noreply.github.com>
2025-04-01 12:26:34 +0200

    qa: add test to verify valid `oxid.meta.php` generation
    The current implementation generates completely broken code. This verifies that the generated `oxid.meta.php` file does actually work again.
    
    Signed-off-by: Maximilian Bösing <2189546+boesing@users.noreply.github.com>

---
+++ tests/Integration/testData/Valid/oxid.meta.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Used by PhpStorm to map factory methods to classes for code completion, source code analysis, etc.
+ *
+ * The code is not ever actually executed and it only needed during development when coding with PhpStorm.
+ *
+ * @see https://confluence.jetbrains.com/display/PhpStorm/PhpStorm+Advanced+Metadata
+ */
+
+namespace PHPSTORM_META {
+    override(\oxNew(0), type(0));
+}
+
+// Simulating the iteration in PHP for moduleParentClasses
+
+namespace OxidEsales\TestModule\Model {
+    class User_parent extends \OxidEsales\Eshop\Application\Model\User
+    {}
+}
+
+namespace OxidEsales\TestModule\Model {
+    class Article_parent extends \OxidEsales\Eshop\Application\Model\Article
+    {}
+}
